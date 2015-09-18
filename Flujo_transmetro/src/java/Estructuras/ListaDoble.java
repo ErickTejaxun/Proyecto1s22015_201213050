636f89f37be7fff8ee3abdc55c180ef4dc2c8059
ErickTejaxun <erickteja@gmail.com>
2015-09-17 18:52:49 -0600

    SSSSS

--- Flujo_transmetro/src/java/Estructuras/ListaDoble.java
+++ Flujo_transmetro/src/java/Estructuras/ListaDoble.java
@@ -86,6 +86,25 @@ public class ListaDoble {
         }
     }
     
+    public void EliminarEstacion(int clave)
+    {
+        NodoLista temp=this.cola;
+        this.Vaciar();
+        while(temp!=null)
+        {
+            Estacion aux=(Estacion)temp.data;
+            if(aux.id==clave)
+            {
+               System.out.println(aux.id);
+               temp=temp.ptr_siguiente;
+            }
+            Estacion aux2=(Estacion)temp.data;
+                this.InsertarInicio(aux2);
+                temp=temp.ptr_siguiente;  
+        }   
+    }
+    
+    
     public void Eliminar(int clave)
     {
         NodoLista temp=this.cola;
@@ -142,6 +161,47 @@ public class ListaDoble {
         Runtime.getRuntime().exec(cmd);
     }
     
+    public void ImprimirEstacion()throws IOException
+    {
+        String nombre_archivo="ListaEstaciones";
+        FileWriter fichero = new FileWriter(this.PathActual()+"\\"+nombre_archivo+".dot");
+        PrintWriter pw = new PrintWriter(fichero);
+        pw.println( "digraph Buses{"                
+        + "node [shape = record,height=.1];");        
+        
+        //Recorrido de la lista
+        //tipos: 1 Estacion Clave
+        //       2 Estacion General
+        //        Nombre  y ID 
+        
+        NodoLista temp=this.cola;
+        while(temp.ptr_siguiente!=null)
+        {
+            Estacion aux=(Estacion)temp.data;
+            Estacion aux2=(Estacion)temp.ptr_siguiente.data;
+            String tipo1,tipo2="";
+            if(aux.tipo==1){tipo1="Clave";}else{tipo1="General";}
+            if(aux2.tipo==1){tipo2="Clave";}else{tipo2="General";}
+            pw.println("\"Nombre:"+aux.nombre+"\nTipo:"+tipo1+"\nID:"+aux.id+"\"->"+"\"Nombre:"+aux2.nombre+"\nTipo:"+tipo2+"\nID:"+aux2.id+"\"");
+            temp=temp.ptr_siguiente;
+        }
+        temp=this.cabeza;
+        while(temp.ptr_anterior!=null)
+        {
+            Estacion aux=(Estacion)temp.data;
+            Estacion aux2=(Estacion)temp.ptr_anterior.data;
+            String tipo1,tipo2="";
+            if(aux.tipo==1){tipo1="Clave";}else{tipo1="General";}
+            if(aux2.tipo==1){tipo2="Clave";}else{tipo2="General";}
+            pw.println("\"Nombre:"+aux.nombre+"\nTipo:"+tipo1+"\nID:"+aux.id+"\"->"+"\"Nombre:"+aux2.nombre+"\nTipo:"+tipo2+"\nID:"+aux2.id+"\"");
+            temp=temp.ptr_anterior;
+        }
+        pw.println("}"); 
+        fichero.close();
+        //Crear el archivo jpg para mostrar.
+        String  cmd = "dot "+PathActual()+"\\"+nombre_archivo+".dot "+"-Tpng "+"-o "+PathActual()+"\\"+nombre_archivo+".png"; //Crear imagen.
+        Runtime.getRuntime().exec(cmd);
+    }
     
     public String PathActual()
     {
